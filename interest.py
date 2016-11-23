75a46f95ac879c5193a31d5a6db8c5e9af6a65b4
Rohit Kumar Yadav <rohit.yadav848@yahoo.com>
2016-11-23 22:26:03 +0530

    interest

--- interest.py
+++ interest.py
@@ -3,6 +3,6 @@ r = int(input("Enter the rate: "))
 t = int(input("Enter the number of months: "))
 interest = (p*r)/100
 for x in range(t):
-	total_money = p + interest
-	p = total_money
+	p += interest
+
 print(total_money)
