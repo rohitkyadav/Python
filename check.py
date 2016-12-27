c7e1d261d208b5ee972f19e50e7c54ddd6e4e848
Rohit Kumar Yadav <rohit.yadav848@yahoo.com>
2016-12-27 14:50:37 +0530

    datetime code

--- check.py
+++ check.py
@@ -1,7 +1,9 @@
 import datetime
+
 today = datetime.date.today()
 print(today.day)
 print(today.strftime("today's date is %d %b %Y"))
 print(today.strftime("today's date is %d %m %y"))
+
 todaytime = datetime.datetime.now()
 print(todaytime.minute)
