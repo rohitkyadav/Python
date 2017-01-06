ca0c8e1847f588f2dadd9eaaf97aaa2c7479bc62
Rohit Kumar Yadav <rohit.yadav848@yahoo.com>
2017-01-06 18:26:16 +0530

    scraping with python

--- scraping.py
+++ scraping.py
@@ -2,17 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_data(max_pages):
-	
 	page = 1
-	
-	
 	while page <= max_pages:
-		
 		url = "http://delhi-ncr.yellowpages.co.in/Coffee?trc=336&page=" + str(page)
 		r = requests.get(url)
 		soup = BeautifulSoup(r.content)   #soup holds all the content in the above link
 		#print (soup.prettify())
 		#soup.find_all('a')  #gives all the links  # a is a HTML tag
+		
 		#for link in soup.find_all("a"):
 			#print(link)
 
@@ -25,12 +22,10 @@ def get_data(max_pages):
 		#for link in soup.find_all("a"):    
 			#print(link.text,link.get("href"))
 
-
 		for link in soup.find_all("a"):
 			#if "http" in link.get("href"):
 			print("<a href='%s'>%s</a>" %(link.get("href"),link.text))
 			
-
 		g_data = soup.find_all("li", {"class":"clearfix"})
 
 		for item in g_data:
@@ -39,8 +34,7 @@ def get_data(max_pages):
 				print (item.contents[3].find_all('div',{"class":"phone"})[0].text)
 			except:
 				pass
-
+		
 		page += 1
-
 
 get_data(2)
